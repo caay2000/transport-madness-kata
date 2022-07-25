7f6614d7e0e9e716f83ef5900b9c69013746c912
Albert Casanovas <caay2000@gmail.com>
2022-07-25 13:09:24 +0200

    * Updating Readme with Exercise 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     application
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.7.10"
     id("info.solidsoft.pitest") version "1.7.4"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
 }
@@ -26,33 +26,33 @@ tasks.test {
 
 dependencies {
     implementation("org.jgrapht:jgrapht-core:1.5.1")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.10")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.10")
     implementation("io.arrow-kt:arrow-core:1.0.1")
 
     testImplementation(kotlin("test"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
     testImplementation("org.assertj:assertj-core:3.21.0")
     testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
     testImplementation("com.google.code.gson:gson:2.9.0")
+    testImplementation("io.kotest.extensions:kotest-assertions-arrow:1.0.2")
 }
 
-apply(plugin = "info.solidsoft.pitest.aggregator")
 pitest {
-    pitestVersion.set("1.8.0")
-    junit5PluginVersion.set("0.16")
+    pitestVersion.set("1.9.3")
+    junit5PluginVersion.set("1.0.0")
     targetClasses.add("com.github.caay2000.ttk.*")
     excludedMethods.addAll("toString")
-    outputFormats.addAll("XML", "HTML")
-    timestampedReports.set(false)
-    exportLineCoverage.set(true)
+    excludedTestClasses.add("**.*IntegrationTest")
+    outputFormats.addAll("HTML")
     avoidCallsTo.add("kotlin.jvm.internal")
     mutators.addAll("STRONGER", "EXTENDED")
     detectInlinedCode.set(true)
+    timestampedReports.set(false)
     threads.set(4)
     failWhenNoMutations.set(false)
     dependencies {
-        pitest("com.groupcdg.pitest:pitest-accelerator-junit5:0.1.0")
-        pitest("com.groupcdg.pitest:extended-mutators:0.1.2")
-        pitest("com.groupcdg.pitest:pitest-kotlin-plugin:0.1.1")
+        pitest("com.groupcdg.pitest:pitest-accelerator-junit5:1.0.0")
+        pitest("com.groupcdg.pitest:extended-mutators:0.1.4")
+        pitest("com.groupcdg.pitest:pitest-kotlin-plugin:0.1.2")
     }
 }
